d2e4b2af672f83654e662d749969775f11fd0fda
David Guthmann <drguthmann@cox.net>
2020-11-10 19:49:21 -0600

    adding in pictures and removing copyright symbol in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 export default function Footer(props) {
-    const copyRSymbol = "&#169"
+    // const copyRSymbol = "&#169"
+    
     return (
-    <footer>{copyRSymbol}Copyright 2020 David Guthmann</footer>
+    <footer>Copyright 2020 David Guthmann</footer>
     )
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Project(props) {
     return (
-        <div className="col-md-6 repo2">
+        <div className="col-md-6">
             <img src={props.projectImage} className="img-responsive" />
             <a className="porttext" target="_blank" href={props.liveUrl}>{props.liveTag}</a>
             <a className="porttext" target="_blank" href={props.gitUrl}>{props.gitTag}</a>
